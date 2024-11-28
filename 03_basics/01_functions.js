fd83e198068c912d05f2b237e4fd401c8177c1fd
Usha Dhamiwal <124116190+usha-dhamiwal@users.noreply.github.com>
2024-11-28 16:56:51 +0000

    taking parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,37 @@ function loginUserMessage(username = "Sam") {
 }
 
 // console.log(loginUserMessage("Usha"))
-console.log(loginUserMessage("Usha"));
+// console.log(loginUserMessage("Usha"));
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+  username: "Usha",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user)
+
+handleObject({
+  username: "Sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
