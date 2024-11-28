8ae0299a46724e97f37b0e213875acd641daa2ab
Usha Dhamiwal <124116190+usha-dhamiwal@users.noreply.github.com>
2024-11-28 17:35:14 +0000

    scope this and arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,49 @@
+var c = 300;
+let a = 300;
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  //   console.log("INNER: ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+  const username = "usha";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //   console.log(website);
+  two();
+}
+
+// one()
+
+if (true) {
+  const username = "usha";
+  if (username == "usha") {
+    const website = " youtube";
+    // console.log(username + website)
+  }
+  // console.log(website)
+}
+
+// console.log(username)
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num) {
+  return num + 1;
+}
+
+addtwo(5);
+const addtwo = function (num) {
+  return num + 2;
+};
