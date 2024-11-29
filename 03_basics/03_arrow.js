f65fa7b62ece8e18114a461bc3f5f06ecb595992
Usha Dhamiwal <124116190+usha-dhamiwal@users.noreply.github.com>
2024-11-29 18:20:43 +0000

    scope this and arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,47 @@
+const user = {
+  username: "usha",
+  price: 999,
+  welcomeMessage: function () {
+    console.log(`${this.username}, welcome to website`);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//     let username = "usha"
+//     console.log(this.username)
+// }
+
+// chai()
+
+// const chai = function() {
+//     let username = "usha"
+//     console.log(this.username)
+// }
+
+const chai = () => {
+  let username = "usha";
+  console.log(this);
+};
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// const addTwo = (num1, num2) => num1 + num2;
+// const addTwo = (num1, num2) => (num1 + num2);
+const addTwo = (num1, num2) => ({ username: "usha" });
+
+console.log(addTwo(3, 4));
+
+const myArray = [2, 5, 3, 7, 8];
+
+// myArray.forEach()
