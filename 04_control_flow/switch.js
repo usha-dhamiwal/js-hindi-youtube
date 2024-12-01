c4551d668290d9196d9518ef36ba2ddb4ae9ad60
Usha Dhamiwal <124116190+usha-dhamiwal@users.noreply.github.com>
2024-12-01 17:25:53 +0000

    control flow code files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,46 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("Feb");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   default:
+//     console.log("default case match");
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("Feb");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+  default:
+    console.log("default case match");
+}
