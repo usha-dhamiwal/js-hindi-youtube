6340aa6adbe26bc0dcf32b1db31f39263979f100
Usha Dhamiwal <124116190+usha-dhamiwal@users.noreply.github.com>
2024-12-03 19:59:59 +0000

    loops discussion

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -6,4 +6,5 @@ const newNums = myNumbers
                 .map((num) => num * 10)
                 .map((num) => num + 1)
                 .filter((num) => num >= 40);
+                
 console.log(newNums);
